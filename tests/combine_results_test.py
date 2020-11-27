6dfd8a6a7c91b2bdb01bfc597d2510d263635728
Victoria Carr <victoriacarr018@gmail.com>
2020-11-27 15:54:04 +0000

    add more unit tests

--- tests/combine_results_test.py
+++ tests/combine_results_test.py
@@ -2,7 +2,7 @@ import io
 import unittest
 import argparse
 
-from bin.combine_results import get_sero_res_contents, get_arguments
+from bin.combine_results import get_content_with_id, get_sero_res_contents, get_arguments
 
 class TestCombineResults(unittest.TestCase):
 
@@ -10,7 +10,15 @@ class TestCombineResults(unittest.TestCase):
     TEST_DATA_SEROTYPE = 'test_data/' + TEST_LANE + '_SeroType_Results.txt'
     TEST_DATA_RES_INCIDENCE = 'test_data/' + TEST_LANE + '_res_incidence.txt'
     TEST_DATA_RES_ALLELES = 'test_data/' + TEST_LANE + '_res_alleles.txt'
-    TEST_DATA_RES_VARIANS = 'test_data/' + TEST_LANE + '_res_gbs_variants.txt'
+    TEST_DATA_RES_VARIANTS = 'test_data/' + TEST_LANE + '_res_gbs_variants.txt'
+
+    def test_should_get_content_with_id_alleles(self):
+        actual = get_content_with_id(self.TEST_LANE, self.TEST_DATA_RES_ALLELES)
+        self.assertEqual(actual, "ID\tEC\tFQ\tOTHER\tTET\n25292_2#85\tERMB(ERMB-1):MEF(MEF-1):23S1:23S3\tPARC:GYRA-V1A,M2Q,G3W,K4W\tMsrD_MLS(MsrD_296):Ant6-Ia_AGly(Ant6-Ia_1633):Sat4A_AGly(Sat4A_586):Aph3-III_AGly(Aph3-III_268):msr(D)(msr(D)_2):ant(6)-Ia(ant(6)-Ia):aph(3')-III(aph(3')-III)\tTETO(TETO-1)\n")
+
+    def test_should_get_content_with_id_variants(self):
+        actual = get_content_with_id(self.TEST_LANE, self.TEST_DATA_RES_VARIANTS)
+        self.assertEqual(actual, "ID\t23S1\t23S3\tGYRA\tPARC\tRPOBGBS-1\tRPOBGBS-2\tRPOBGBS-3\tRPOBGBS-4\n25292_2#85\t23S1\t23S3\tGYRA-V1A,M2Q,G3W,K4W\tPARC\t\t\t\t\n")
 
     def test_should_get_sero_res_contents(self):
         actual = get_sero_res_contents(self.TEST_LANE, self.TEST_DATA_SEROTYPE, self.TEST_DATA_RES_INCIDENCE)
