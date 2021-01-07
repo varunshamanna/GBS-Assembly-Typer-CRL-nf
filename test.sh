ccb670aa9004e8ea453d95a73f3c2bae4a649dca
Victoria Carr <victoriacarr018@gmail.com>
2021-01-07 16:08:24 +0000

    update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ in_dir=test_data/input_data
 out_dir=test_data/output_data
 
 # Run pipeline on test input data
-nextflow run main.nf --reads "$in_dir/test_{1,2}.fastq.gz" --output "$out_dir/test"
+nextflow run main.nf --reads "$in_dir/test_{1,2}.fastq.gz" --results_dir "$out_dir" --output 'test'
 
 function file_diff {
     local test=${1}
@@ -20,20 +20,19 @@ function file_diff {
 
 error_status=0
 # Check for test_drug_cat_alleles.txt output
-file_diff ${out_dir}/test_drug_cat_alleles.txt ${out_dir}/reference_drug_cat_alleles.txt
-error_status=$?
+file_diff ${out_dir}/test_drug_cat_alleles_variants.txt ${out_dir}/reference_drug_cat_alleles_variants.txt
+out=$?
+error_status=$(($error_status | $out))
 
 # Check for test_gbs_res_variants.txt output
 file_diff ${out_dir}/test_gbs_res_variants.txt ${out_dir}/reference_gbs_res_variants.txt
-if [ $error_status = 0 ]; then
-    error_status=$?
-fi
+out=$?
+error_status=$(($error_status | $out))
 
 # Check for test_serotype_res_incidence.txt output
 file_diff ${out_dir}/test_serotype_res_incidence.txt ${out_dir}/reference_serotype_res_incidence.txt
-if [ $error_status = 0 ]; then
-    error_status=$?
-fi
+out=$?
+error_status=$(($error_status | $out))
 
 # Error if any output files missing or not expected
 if [ $error_status = 1 ]; then
